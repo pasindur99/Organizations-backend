a9d37f88261d718cf10bad33147df047cdf28281
Pasindu Rupasinghe <77311602+pasindur99@users.noreply.github.com>
2021-07-21 17:18:21 +0530

    first db connection practice success (#1)

---
+++ src/main/java/com/demo/services/OrgDao.java
@@ -0,0 +1,79 @@
+/*
+1. import -- > java.sql
+2. load and register the driver -- > com.mysql.jdbc.Driver
+3. create connection
+4. create a statement
+5. execute the query
+7. close
+ */
+package com.demo.services;
+import com.demo.entities.Organization;
+import java.sql.*;
+
+public class OrgDao {
+
+    public Organization getOrg(int id){
+
+        Organization org = new Organization();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/organizations","root","pas.pas.pas.");
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from organization");
+            if(rs.next()){
+                org.setId(rs.getInt("id"));
+                org.setName(rs.getString("name"));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return org;
+
+    }
+/*
+    public int addOrganiaztion(Organization org) throws ClassNotFoundException{
+
+        String INSERT_SQL = "INSERT INTO organization" + "(id , name )";
+
+        int result = 0;
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/mysql_database?useSSL=false", "root", "pas.pas.pas.");
+
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_SQL)){
+
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, org.getName());
+
+            System.out.println(preparedStatement);
+            result = preparedStatement.executeUpdate();
+
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        return result;
+    }
+
+    private void printSQLException(SQLException ex) {
+
+        for(Throwable e: ex){
+            if (e instanceof SQLException){
+
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+ */
+
+}
---
+++ src/main/webapp/showOrganization.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: pasindu
+  Date: 2021-07-20
+  Time: 08.42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page import="com.demo.entities.Organization" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Organizations</title>
+</head>
+<body>
+
+<h1>In organizaton page </h1>
+<%
+    Organization org1 = (Organization) request.getAttribute("Organizations");
+    out.println(org1);
+%>
+
+</body>
+</html>
