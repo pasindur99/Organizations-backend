88848b3ffedcb54efecee8e62f383f6225f2d030
pasindur2 <pasindur2pj@gmail.com>
2021-09-30 09:55:06 +0530

    Rename java servlet dependency

--- src/main/java/com/util/ServletUtil.java
+++ src/main/java/com/util/ServletUtil.java
@@ -1,8 +1,8 @@
 package com.util;
 
 import com.google.gson.Gson;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
